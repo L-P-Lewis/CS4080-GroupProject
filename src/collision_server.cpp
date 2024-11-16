f719f490588011b7c84486220edb753f1753bf72
Creamsicle42 <defuego424242@gmail.com>
2024-11-16 07:11:22 -0800

    Added basic collision server implementation

---
+++ src/collision_server.cpp
@@ -0,0 +1,14 @@
+#include <vector>
+#include "collision_server.h"
+
+using namespace KinSolver;
+
+int CollisionServer::RegisterShape(Shape NewShape) {
+	// Scan through shapes vector to find an empty space
+	int TargetPos;
+	for (int i = 0; i < Shapes.size(); i++) {
+		if (Shapes[i] == ) {
+			TargetPos == i;
+		}
+	}
+}
